c2d76ce4571f0c9c6e93e81d6a3f17de354312d4
CorySchulz <coryschulz@gmail.com>
2024-11-23 01:03:42 -0300

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,60 @@
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import serve from 'rollup-plugin-serve';
+import copy from 'rollup-plugin-copy';
+import postcss from 'rollup-plugin-postcss';
+
+const dev = process.env.ROLLUP_WATCH;
+
+export default [
+	// Development build
+	{
+		input: 'src/modal-panel.js',
+		output: {
+			file: 'dist/modal-panel.js',
+			format: 'iife',
+			sourcemap: dev,
+		},
+		plugins: [
+			resolve(),
+			postcss({
+				extract: 'modal-panel.min.css',
+				minimize: true,
+			}),
+			dev &&
+				serve({
+					contentBase: ['dist', 'demo'],
+					open: true,
+					port: 3000,
+				}),
+			copy({
+				targets: [
+					{ src: 'dist/modal-panel.js', dest: 'demo' },
+					{ src: 'dist/modal-panel.min.css', dest: 'demo' },
+				],
+				hook: 'writeBundle',
+			}),
+		],
+	},
+	// Production build (minified)
+	{
+		input: 'src/modal-panel.js',
+		output: {
+			file: 'dist/modal-panel.min.js',
+			format: 'iife',
+			sourcemap: false,
+		},
+		plugins: [
+			resolve(),
+			postcss({
+				extract: 'dist/modal-panel.min.css', // Minified CSS output
+				minimize: true,
+			}),
+			terser({
+				format: {
+					comments: false,
+				},
+			}),
+		],
+	},
+];
---
+++ src/modal-panel.js
@@ -0,0 +1,78 @@
+class ModalPanel extends HTMLElement {
+	constructor() {
+		super();
+
+		this.id = this.getAttribute('id');
+		this.content = this.querySelector('modal-content');
+		this.setAttribute('aria-hidden', 'true');
+
+		this.prepend(document.createElement('modal-overlay'));
+
+		this.bindUI();
+	}
+
+	bindUI() {
+		document.addEventListener('click', (e) => {
+			if (!e.target.closest(`[aria-controls="${this.id}"]`)) return;
+			if (e.target.getAttribute('data-prevent-default') == 'true') {
+				e.preventDefault();
+			}
+			this.show();
+		});
+
+		this.addEventListener('click', (e) => {
+			if (!e.target.closest('[data-action="hide-modal"]')) return;
+			this.hide();
+		});
+	}
+
+	show() {
+		this.setAttribute('aria-hidden', 'false');
+		this.classList.remove('hide');
+		document.body.classList.add('overflow-hidden');
+
+		// Focus the first focusable element inside the modal-content
+		const firstFocusable = this.querySelector(
+			'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+		);
+		if (firstFocusable) {
+			firstFocusable.focus();
+		}
+	}
+
+	hide() {
+		// Trigger hide animations
+		this.classList.add('hide');
+		document.body.classList.remove('overflow-hidden');
+		setTimeout(() => {
+			this.setAttribute('aria-hidden', 'true');
+			this.classList.remove('hide');
+		}, 400);
+	}
+}
+
+class ModalOverlay extends HTMLElement {
+	constructor() {
+		super();
+		// Make sure this is focusable
+		this.setAttribute('tabindex', 0);
+		this.modalPanel = this.closest('modal-panel');
+		this.bindUI();
+	}
+
+	bindUI() {
+		this.addEventListener('click', () => {
+			this.modalPanel.hide();
+		});
+	}
+}
+
+class ModalContent extends HTMLElement {
+	constructor() {
+		super();
+	}
+}
+
+customElements.define('modal-panel', ModalPanel);
+customElements.define('modal-overlay', ModalOverlay);
+customElements.define('modal-content', ModalContent);
